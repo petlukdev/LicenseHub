df2ed2f094cab49e2443558fed9bfb12b6c631ad
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-04-04 15:05:57 +0200

    DB - Created AppDbContext for future DB manipulation operations

---
+++ LicenceHub/DB/AppDbContext.cs
@@ -0,0 +1,102 @@
+﻿using LicenseHub.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LicenseHub.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<License> Licenses { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string folder = Path.Combine(appDataPath, "LicenseHub");
+            Directory.CreateDirectory(folder);
+            string dbPath = Path.Combine(folder, "licensehub.db");
+
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<License>(entity =>
+            {
+                entity.Property(e => e.Title)
+                      .IsRequired()
+                      .HasMaxLength(150);
+
+                entity.Property(e => e.Key)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+                entity.Property(e => e.Type)
+                      .IsRequired()
+                      .HasConversion<string>();
+
+                entity.Property(e => e.Cost)
+                      .IsRequired();
+
+                entity.Property(e => e.ExpirationDate)
+                      .IsRequired();
+
+                entity.Property(e => e.ExpirationStatus)
+                      .HasConversion<string>();
+
+                entity.HasOne(e => e.Owner)
+                      .WithMany(o => o.Licenses)
+                      .HasForeignKey(e => e.OwnerId);
+
+                entity.HasOne(e => e.Supplier)
+                      .WithMany(s => s.Licenses)
+                      .HasForeignKey(e => e.SupplierId);
+            });
+
+            modelBuilder.Entity<Owner>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                      .IsRequired()
+                      .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                      .IsRequired()
+                      .HasMaxLength(50);
+
+                entity.Property(e => e.Email)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+                entity.HasOne(e => e.Department)
+                      .WithMany(d => d.Owners)
+                      .HasForeignKey(e => e.DepartmentId);
+            });
+
+            modelBuilder.Entity<Supplier>(entity =>
+            {
+                entity.Property(e => e.Name)
+                      .IsRequired()
+                      .HasMaxLength(150);
+
+                entity.Property(e => e.ContactEmail)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+                entity.Property(e => e.ContactPhone)
+                      .IsRequired()
+                      .HasMaxLength(17);
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.Property(e => e.Name)
+                      .IsRequired()
+                      .HasMaxLength(255);
+            });
+        }
+    }
+}
