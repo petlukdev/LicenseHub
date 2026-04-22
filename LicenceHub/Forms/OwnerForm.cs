b099b166191f5d680bba595ce87638d3a45e37fe
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-04-22 21:44:00 +0200

    OwnerForm - Added submit and cancel functionality

--- LicenceHub/Forms/OwnerForm.cs
+++ LicenceHub/Forms/OwnerForm.cs
@@ -1,4 +1,5 @@
-﻿using LicenseHub.Models;
+﻿using LicenseHub.Extensions;
+using LicenseHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,25 +7,90 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LicenseHub.Forms
 {
     public partial class OwnerForm : Form
     {
+        private int _originalId = -1;
+
+        public Owner? Result { get; private set; }
+
         public OwnerForm()
         {
             InitializeComponent();
+            this.Text = "Add Owner";
+
+            // TODO: Load departments from database and populate comboDepartment
         }
 
         public OwnerForm(Owner owner) : this()
         {
             ArgumentNullException.ThrowIfNull(owner, nameof(owner));
 
+            this.Text = "Edit Owner";
+            _originalId = owner.Id;
             txtFirstName.Text = owner.FirstName;
             txtLastName.Text = owner.LastName;
             txtEmail.Text = owner.Email;
         }
 
-        public Owner? Result { get; private set; }
+        private void AddDepartmentEvent(object sender, EventArgs e)
+        {
+            // TODO: Implement department management form and logic
+        }
+
+        private void ApplyEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                string firstName = txtFirstName.Text.Trim();
+                string lastName = txtLastName.Text.Trim();
+                string email = txtEmail.Text.Trim();
+                Department? department = comboDepartment.SelectedItem as Department;
+
+                if (string.IsNullOrEmpty(firstName))
+                    throw new ArgumentNullException(nameof(firstName));
+                if (string.IsNullOrEmpty(lastName))
+                    throw new ArgumentNullException(nameof(lastName));
+                if (string.IsNullOrEmpty(email))
+                    throw new ArgumentNullException(nameof(email));
+
+                if (firstName.Length > 50)
+                    throw new ArgumentException("First name cannot be longer than 50 characters.");
+                if (lastName.Length > 50)
+                    throw new ArgumentException("Last name cannot be longer than 50 characters.");
+                if (email.Length > 255)
+                    throw new ArgumentException("Email cannot be longer than 255 characters.");
+
+                this.Result = _originalId == -1
+                    ? new Owner { 
+                        FirstName = firstName, 
+                        LastName = lastName, 
+                        Email = email, 
+                        Department = department 
+                    }
+                    : new Owner { 
+                        Id = _originalId, 
+                        FirstName = firstName, 
+                        LastName = lastName, 
+                        Email = email, 
+                        Department = department 
+                    };
+
+                this.DialogResult = DialogResult.OK;
+            }
+            catch (Exception ex)
+            {
+                MessageViewer.ShowError("An error occurred while trying to make new owner.", ex.Message);
+            }
+        }
+
+        private void CancelEvent(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
     }
 }
