cc43371fc8fbab4f3757fe266e89f9d97737feed
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-04-26 18:23:21 +0200

    Extensions - Added new ComboBoxExtensions setup method

--- LicenceHub/Extensions/ComboBoxExtensions.cs
+++ LicenceHub/Extensions/ComboBoxExtensions.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace LicenseHub.Extensions
 {
     public static class ComboBoxExtensions
     {
-        public static void Setup<T>(this ComboBox comboBox, LocalView<T> items, Func<T, FilterItem> map, string allText) where T : class
+        public static void Setup<T>(
+            this ComboBox comboBox,
+            LocalView<T> items,
+            Func<T, FilterItem> map,
+            string allText
+            ) where T : class
         {
             void UpdateData()
             {
@@ -22,6 +28,36 @@ namespace LicenseHub.Extensions
 
             UpdateData();
             items.CollectionChanged += (sender, e) => UpdateData();
+        }
+
+        public static void SetupOptionalData<T>(
+            this ComboBox comboBox,
+            IBindingList list,
+            T emptyItem,
+            string displayMember = "Name",
+            string valueMember = "Id"
+            ) where T : class
+        {
+            void UpdateData()
+            {
+                var originalValue = comboBox.SelectedValue;
+
+                var displayList = list.Cast<T>().ToList();
+                displayList.Insert(0, emptyItem);
+
+                comboBox.DataSource = null;
+                comboBox.DataSource = displayList;
+                comboBox.DisplayMember = displayMember;
+                comboBox.ValueMember = valueMember;
+
+                if (originalValue != null)
+                {
+                    comboBox.SelectedValue = originalValue;
+                }
+            }
+
+            UpdateData();
+            list.ListChanged += (sender, e) => UpdateData();
         }
     }
 }
